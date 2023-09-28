1205e5068d0c0cb9b6a6f25a12454b3305d96c01
nthnn <nathanneisip@gmail.com>
2023-09-28 17:36:38 +0800

    Sign-up and log-in UI page.

--- views/log-in.php
+++ views/log-in.php
@@ -1,1 +1,39 @@
-<h1>Log-in</h1>
+<div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+    <div class="container animate__animated animate__slideInDown" align="center">
+        <div class="col-lg-8" align="left">
+            <div class="card card-body border-primary">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mobile-only">
+                            <center>
+                                <img src="assets/qlbase-logo.png" width="50%" />
+                                <h1 class="mt-2">Log-in</h1>
+                            </center>
+                        </div>
+
+                        <h1 class="desktop-only">Log-in</h1>
+                        <hr/>
+
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" id="username" class="form-control" placeholder="Username" />
+
+                        <label for="password" class="form-label mt-2">Password</label>
+                        <input type="password" id="password" class="form-control" placeholder="Password" />
+                        <br/>
+
+                        <button class="btn btn-primary w-100">Log-in</button>
+                        <hr/>
+
+                        <p align="center">Or <a href="?page=sign-up" class="text-decoration-none">sign-up</a> if you don't have an account yet.</p>
+                    </div>
+
+                    <div class="col-lg-6 d-flex justify-content-center align-items-center" align="center">
+                        <img src="assets/qlbase-logo.png" class="desktop-only" width="80%" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="scripts/login.js"></script>
--- views/sign-up.php
+++ views/sign-up.php
@@ -1,1 +1,39 @@
-<h1>Sign-up</h1>
+<div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+    <div class="container animate__animated animate__slideInDown" align="center">
+        <div class="col-lg-8" align="left">
+            <div class="card card-body border-primary">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mobile-only">
+                            <center>
+                                <img src="assets/qlbase-logo.png" width="50%" />
+                                <h1 class="mt-2">Sign-up</h1>
+                            </center>
+                        </div>
+
+                        <h1 class="desktop-only">Sign-up</h1>
+                        <hr/>
+
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" id="username" class="form-control" placeholder="Username" />
+
+                        <label for="password" class="form-label mt-2">Password</label>
+                        <input type="password" id="password" class="form-control" placeholder="Password" />
+                        <br/>
+
+                        <button class="btn btn-primary w-100">Sign-up</button>
+                        <hr/>
+
+                        <p align="center">Or <a href="?page=log-in" class="text-decoration-none">log-in</a> if you already have an account.</p>
+                    </div>
+
+                    <div class="col-lg-6 d-flex justify-content-center align-items-center" align="center">
+                        <img src="assets/qlbase-logo.png" class="desktop-only" width="80%" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="scripts/login.js"></script>
