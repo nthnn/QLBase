d4ca2ae3be2dcb8eb98a711582d969298bf8b3e9
nthnn <nathanneisip@gmail.com>
2023-09-29 23:17:40 +0800

    Database table declarations.

---
+++ db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+);
+
+CREATE TABLE `sessions` (
+  `user_id` int(11) DEFAULT NULL,
+  `hash` varchar(255) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `remote_addr` varchar(255) DEFAULT NULL
+);
