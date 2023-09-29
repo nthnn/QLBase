78368fb5197ffc43fb792f296817a04f78c461b2
nthnn <nathanneisip@gmail.com>
2023-09-29 23:25:14 +0800

    Dashboard navigation bar.

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,1 +1,35 @@
-<h1>Hello</h1>
+<nav class="navbar navbar-expand-lg bg-primary fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">QLBase</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar-main">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">
+                        Dashboard
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=settings">
+                        Settings
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="logout" href="#">
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<br/><br/><br/>
+
+<div class="container">
+    <h1>Hello!</h1>
+</div>
+
+<script src="scripts/vendors/bootstrap.bundle.min.js"></script>
