e396f497c7d2569138e21954d3d4fc912d734990
nthnn <nathanneisip@gmail.com>
2023-10-01 19:59:28 +0800

    App table on database.

--- db.sql
+++ db.sql
@@ -1,9 +1,17 @@
 CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
+);
+
+CREATE TABLE `app` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `creator_id` int(11),
+  `app_id` varchar(255) DEFAULT NULL,
+  `app_key` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL
 );
 
 CREATE TABLE `sessions` (
