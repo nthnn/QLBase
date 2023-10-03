54e3c171e53e3908b6363f0fdaaaaf1ef89c72d8
nthnn <nathanneisip@gmail.com>
2023-10-04 00:31:45 +0800

    Improved app overview page.

--- views/appsec/overview.php
+++ views/appsec/overview.php
@@ -10,7 +10,21 @@ $userName = getAccountUsername($userId);
 <h1 class="border-bottom">Overview</h1>
 <br/>
 
-<div class="row">
+<div class="row" align="center">
+    <div class="col-sm-6">
+        <h3 class="text-primary mb-0"><?php echo $appInfo["app_name"]; ?></h3>
+        <small class="text-muted">App Name</small>
+        <br/><br/>
+    </div>
+
+    <div class="col-sm-6">
+        <h2 class="text-primary mb-0"><?php echo $userName; ?></h2>
+        <small class="text-muted">Creator</small>
+    </div>
+</div>
+<br/>
+
+<div class="row" align="center">
     <div class="col-sm-6">
         <h3 class="text-primary mb-0"><?php echo $appInfo["app_key"]; ?></h3>
         <small class="text-muted">App Key</small>
@@ -22,8 +36,4 @@ $userName = getAccountUsername($userId);
         <small class="text-muted">App ID</small>
     </div>
 </div>
-<br/>
-
-<h2 class="text-primary mb-0"><?php echo $userName; ?></h2>
-<small class="text-muted">Creator</small>
 <br/>
