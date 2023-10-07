839b4480be2525ea056327d6c81413b5e82bfb50
nthnn <nathanneisip@gmail.com>
2023-10-07 16:36:47 +0800

    Deployment script.

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+cd backend/auth && ./build.sh && cd ../..
+cd backend/sms && ./build.sh && cd ../..
+zip -r qlbase.zip api assets bin components controller scripts side styles views favicon.ico index.php
