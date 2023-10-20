7f6e812adbc7d224f00dd64254aa4e92f1eca110
nthnn <nathanneisip@gmail.com>
2023-10-20 23:59:47 +0800

    Improved SMS service and UI.

--- scripts/appsec/sms.js
+++ scripts/appsec/sms.js
@@ -1,4 +1,51 @@
+let prevUsersHash = "";
+let deletableVerification = null,
+    deletableOTP = null;
+
+function deleteVerification(recipient, code) {
+    deletableVerification = recipient;
+    deletableOTP = code;
+
+    $("#deletable-verification").html(deletableVerification);
+    $("#deletable-otp").html(deletableOTP);
+    $("#confirm-delete-modal").modal("show");
+}
+
 const fetchSMSLogs = ()=> {
+    $.post({
+        url: "api/index.php?action=fetch_all_otp",
+        headers: {
+            "QLBase-App-ID": App.appId,
+            "QLBase-API-Key": App.appKey
+        },
+        success: (data)=> {
+            if(data.result == '0')
+                return;
+
+            if(prevUsersHash == CryptoJS.MD5(JSON.stringify(data)).toString())
+                return;
+            prevUsersHash = CryptoJS.MD5(JSON.stringify(data)).toString();
+
+            let otpRows = "";
+            const enabilityIcon = {
+                "1": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" width=\"16\" height=\"16\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M4.5 12.75l6 6 9-13.5\" /></svg>",
+                "0": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" width=\"16\" height=\"16\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M6 18L18 6M6 6l12 12\" /></svg>"
+            };
+
+            data.value.forEach((e)=>
+                otpRows += "<tr><td>" + e[0] +
+                    "</td><td>" + e[1] +
+                    "</td><td>" + e[2] +
+                    "</td><td>" + e[3] +
+                    "</td><td>" + enabilityIcon[e[4]] +
+                    "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteVerification('" +
+                    e[0] + "', '" + e[1] + "')\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" width=\"16\" height=\"16\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0\" /></svg>" +
+                    "</button></td></tr>"
+            );
+
+            $("#sms-table-body").html(otpRows);
+        }
+    });
 };
 
 $(document).ready(()=> {
--- views/appsec/sms.php
+++ views/appsec/sms.php
@@ -7,14 +7,48 @@
         <th>Code</th>
         <th>Support Email</th>
         <th>Date Creation</th>
+        <th>Validated</th>
         <th>Options</th>
     </thead>
-    <tbody>
+    <tbody id="sms-table-body">
     </tbody>
 </table>
 
+<div class="modal fade" id="confirm-delete-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-delete-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirm-delete-modalLabel">Delete Verification</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to remove &quot;<span id="deletable-verification"></span>&quot; with OTP &quot;<span id="deletable-otp"></span>&quot; verification?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+
+                <button type="button" class="btn btn-outline-danger" onclick="requestVerificationDeletion()" id="delete-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="scripts/vendors/jquery.min.js"></script>
 <script src="scripts/vendors/jquery.dataTables.min.js"></script>
+<script src="scripts/vendors/cryptojs.core.min.js"></script>
+<script src="scripts/vendors/cryptojs.md5.min.js"></script>
 <script src="scripts/appsec/sms.js"></script>
 
 <style>@import url("styles/jquery.dataTables.min.css");</style>
