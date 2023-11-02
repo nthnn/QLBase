472be06c96320d18c8ffa46f48677e046e943770
nthnn <nathanneisip@gmail.com>
2023-11-03 03:20:21 +0800

    Modified navigation column and footer.

--- documentations/src/components/Footer.vue
+++ documentations/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <hr/>
-        <small align="center">Copyright 2023-Present &copy; <a href="https://github.com/nthnn/QLBase">QLBase</a></small>
+        <small align="center">Copyright 2023-Present &copy; <a href="https://github.com/nthnn/QLBase" class="text-decoration-none">QLBase</a></small>
     </footer>
     <br/>
 </template>
--- documentations/src/components/NavigationColumn.vue
+++ documentations/src/components/NavigationColumn.vue
@@ -8,23 +8,23 @@ import { RouterLink } from 'vue-router';
         <p>Documentations to help you get started with QLBase. A brief introduction, deployment instructions, and donations.</p>
         <div class="btn-group-vertical w-100 rounded">
             <RouterLink to="/" class="btn btn-outline-primary">What is QLBase?</RouterLink>
-            <RouterLink to="/p1-deployment" class="btn btn-outline-primary">Deployment</RouterLink>
-            <RouterLink to="/p2-troubleshooting" class="btn btn-outline-primary">Troubleshooting</RouterLink>
-            <RouterLink to="/p3-contributing" class="btn btn-outline-primary">Contributing and Contributors</RouterLink>
+            <RouterLink to="/deployment" class="btn btn-outline-primary">Deployment</RouterLink>
+            <RouterLink to="/firmware" class="btn btn-outline-primary">The Firmware</RouterLink>
+            <RouterLink to="/contributing" class="btn btn-outline-primary">Contributing and Contributors</RouterLink>
         </div>
     </div>
     <br/>
 
     <div class="card card-body border-primary border shadow">
-        <h3 class="m-0 mb-2">Services</h3>
+        <h3 class="m-0 mb-2">APIs and Services</h3>
         <p>Different decentralizable, scalable, and realiable services by QLBase for individuals, businesses, and enterprise companies.</p>
         <div class="btn-group-vertical w-100 rounded">
-            <RouterLink to="/p4-authentication" class="btn btn-outline-primary">Authentication</RouterLink>
-            <RouterLink to="/p5-sms-verification" class="btn btn-outline-primary">SMS Verification</RouterLink>
-            <RouterLink to="/p6-data-analytics" class="btn btn-outline-primary">Data Analytics</RouterLink>
-            <RouterLink to="/p7-database" class="btn btn-outline-primary">Database</RouterLink>
-            <RouterLink to="/p7-file-storage" class="btn btn-outline-primary">File Storage</RouterLink>
-            <RouterLink to="/p8-machine-learning" class="btn btn-outline-primary">Machine Learning</RouterLink>
+            <RouterLink to="/authentication" class="btn btn-outline-primary">Authentication</RouterLink>
+            <RouterLink to="/sms-verification" class="btn btn-outline-primary">SMS Verification</RouterLink>
+            <RouterLink to="/data-analytics" class="btn btn-outline-primary">Data Analytics</RouterLink>
+            <RouterLink to="/database" class="btn btn-outline-primary">Database</RouterLink>
+            <RouterLink to="/file-storage" class="btn btn-outline-primary">File Storage</RouterLink>
+            <RouterLink to="/machine-learning" class="btn btn-outline-primary">Machine Learning</RouterLink>
         </div>
     </div>
     <br/>
