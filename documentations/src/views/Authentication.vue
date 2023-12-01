5b846a8e817383a9a58c49935a2e21a0341edf4a
nthnn <nathanneisip@gmail.com>
2023-12-01 23:52:10 +0800

    Improved Authentication documentation page.

--- documentations/src/views/Authentication.vue
+++ documentations/src/views/Authentication.vue
@@ -1,9 +1,22 @@
 <template>
     <h3 class="border-bottom">🔐 Authentication</h3>
     <p>QLBase provides a robust authentication system designed to empower you in managing user accounts securely. Within this service, a range of APIs is offered to facilitate the following key functionalities:</p>
-    <ol>
-        <li><b>User Registration:</b> Effortlessly register new users with customizable registration fields. QLBase streamlines the process, allowing you to tailor registration fields to meet specific application requirements.</li>
-        <li><b>Secure User Login:</b> Ensure secure user logins by supporting various authentication methods, including traditional username/password combinations and the convenience of social logins. This flexibility caters to diverse user preferences, enhancing the overall user experience.</li>
+
+    <div class="row">
+        <div class="col-lg-6 col-md-6">
+            <img src="@/assets/qlbase-cat-3.png" width="100%" />
+        </div>
+
+        <div class="col-lg-6 col-md-6">
+            <br class="desktop-only" />
+            <ol>
+                <li><b>User Registration:</b> Effortlessly register new users with customizable registration fields. QLBase streamlines the process, allowing you to tailor registration fields to meet specific application requirements.</li>
+                <li><b>Secure User Login:</b> Ensure secure user logins by supporting various authentication methods, including traditional username/password combinations and the convenience of social logins. This flexibility caters to diverse user preferences, enhancing the overall user experience.</li>
+            </ol>
+        </div>
+    </div>
+
+    <ol start="3">
         <li><b>Account Management:</b> QLBase provides the capability to disable and enable user accounts as needed. When an account is disabled, the user is temporarily restricted from logging in until reenabled. This feature proves particularly valuable for handling suspicious or inactive accounts, offering a proactive approach to account security.</li>
         <li><b>Expanded Functionality:</b> Beyond the mentioned features, QLBase have varieties of action within its authentication-related APIs. These actions are designed to provide a comprehensive and adaptable toolkit, allowing developers to address specific use cases and tailor authentication processes according to their application's unique requirements.</li>
     </ol>
