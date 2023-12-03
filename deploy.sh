996052facfbfde51fea3c6c7607a289f69ee51e1
nthnn <nathanneisip@gmail.com>
2023-12-03 21:22:52 +0800

    Improved deployment script, added other backends.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-echo [+] Building backend apps...
+echo "\033[92m[+]\033[0m Building backend apps..."
 cd backend/auth && ./build.sh && cd ../..
+cd backend/data_analytics && ./build.sh && cd ../..
+cd backend/forgetpass && ./build.sh && cd ../..
 cd backend/sms && ./build.sh && cd ../..
-echo [+] Zipping deployment entity...
+echo "\033[92m[+]\033[0m Zipping deployment entity..."
 cd documentations && npm install && npm run build-only && cd ..
 zip -r qlbase.zip api assets bin components controller docs scripts side styles views favicon.ico index.php sandbox.html
 rm -rf docs/
-echo [+] Deployment-ready successfully generated!
+echo "\033[92m[+]\033[0m Deployment-ready successfully generated!"
