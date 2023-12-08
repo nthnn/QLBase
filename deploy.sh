6b100ac692a5176614ce2a60eda0e3a8598b635d
nthnn <nathanneisip@gmail.com>
2023-12-08 19:45:58 +0800

    Added traffic backend to deployment script.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ cd backend/auth && ./build.sh && cd ../..
 cd backend/data_analytics && ./build.sh && cd ../..
 cd backend/forgetpass && ./build.sh && cd ../..
 cd backend/sms && ./build.sh && cd ../..
+cd backend/traffic && ./build.sh && cd ../..
 echo "\033[92m[+]\033[0m Zipping deployment entity..."
 cd documentations && npm install && npm run build-only && cd ..
 zip -r qlbase.zip api assets bin components controller docs scripts side styles views favicon.ico index.php sandbox.html
