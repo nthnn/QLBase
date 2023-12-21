e2aa1d451f6d9118b2ac0be5eacc320aee7b11fe
nthnn <nathanneisip@gmail.com>
2023-12-21 16:50:56 +0800

    Database back-end initial main.go

--- backend/database/main.go
+++ backend/database/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"os"
+
+	"github.com/nthnn/QLBase/database/proc"
+)
+
+func failOnUmatchedArgSize(size int, args []string) {
+	if len(args) != size {
+		proc.ShowFailedResponse("Invalid parameter arity.")
+		os.Exit(-1)
+	}
+}
+
 func main() {
+	if len(os.Args) < 3 {
+		proc.ShowFailedResponse("Invalid argument arity.")
+		os.Exit(0)
+	}
 }
