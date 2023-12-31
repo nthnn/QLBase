09b8caddfebe8ee31b53a450a7b7ceae3a528ae9
nthnn <nathanneisip@gmail.com>
2024-01-01 05:06:32 +0800

    Database table rendition functionality.

---
+++ scripts/appsec/database.js
@@ -0,0 +1,80 @@
+let prevDBHash = "",
+    deletableDbName = null,
+    dataTable = null;
+
+function deleteDb(name) {
+    deletableDbName = name;
+
+    $("#deletable-name").html(deletableDbName);
+    $("#confirm-delete-modal").modal("show");
+}
+
+function requestDatabaseDeletion() {
+    $.post({
+        url: "api/index.php?action=db_delete",
+        headers: {
+            "QLBase-App-ID": App.appId,
+            "QLBase-API-Key": App.appKey
+        },
+        data: {
+            name: deletableDbName
+        },
+        success: (data)=> {
+            if(data.result == '0')
+                return;
+
+            $("#success-message").html("Deleted database '" + deletableDbName + "' permanently.");
+            $("#confirm-delete-modal").modal("hide");
+            $("#success-modal").modal("show");
+        }
+    }).fail(()=> {
+        $("#confirm-delete-modal").modal("hide");
+        $("#error-message").html("Error trying to permanently delete database '" + deletableDbName + "'.");
+        $("#error-modal").modal("show");
+    });
+}
+
+const fetchDb = ()=> {
+    $.post({
+        url: "api/index.php?action=db_fetch_all",
+        headers: {
+            "QLBase-App-ID": App.appId,
+            "QLBase-API-Key": App.appKey
+        },
+        success: (data)=> {
+            if(data.result == '0')
+                return;
+
+            if(prevDBHash == CryptoJS.MD5(JSON.stringify(data)).toString())
+                return;
+            prevDBHash = CryptoJS.MD5(JSON.stringify(data)).toString();
+
+            if(data.value.length == 0 && (prevDBHash != "" ||
+                prevDBHash == "5e28988ff412b216da4a633fa9ff52f5")) {
+                dataTable.clear().destroy();
+                dataTable = initDataTable("#db-table", "No database found.");
+
+                return;
+            }
+
+            let rows = "";
+            for(let val of data.value)
+                rows += "<tr><td>" + val[0] +
+                    "</td><td>" + val[1] +
+                    "</td><td><button class=\"btn btn-sm btn-outline-danger\" onclick=\"deleteDb('" +
+                    val[0] + "')\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" width=\"16\" height=\"16\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0\" /></svg>" +
+                    "</button></td>"
+
+                dataTable.clear().destroy();
+                $("#db-table-body").html(rows);
+                dataTable = initDataTable("#db-table", "No database found.");
+        }
+    });
+};
+
+$(document).ready(()=> {
+    dataTable = initDataTable("#db-table", "No database found.");
+
+    fetchDb();
+    setInterval(fetchDb, 2000);
+});
--- views/appsec/db.php
+++ views/appsec/db.php
@@ -1,2 +1,100 @@
 <h1>Database Storage</h1>
 <hr/>
+
+<table class="table table-hover" id="db-table">
+    <thead>
+        <th>Name</th>
+        <th>Mode</th>
+        <th>Options</th>
+    </thead>
+    <tbody id="db-table-body">
+    </tbody>
+</table>
+
+<div class="modal fade" id="success-modal" tabindex="-1" role="dialog" aria-labelledby="success-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="success-modalLabel">Success</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="success-message"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="failed-modal" tabindex="-1" role="dialog" aria-labelledby="failed-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="failed-modalLabel">Failed</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="failed-message"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="confirm-delete-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-delete-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirm-delete-modalLabel">Delete Verification</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to permanently delete database &quot;<span id="deletable-name"></span>&quot;?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+
+                <button type="button" class="btn btn-outline-danger" onclick="requestDatabaseDeletion()" id="delete-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="scripts/vendors/jquery.min.js"></script>
+<script src="scripts/vendors/jquery.dataTables.min.js"></script>
+<script src="scripts/vendors/cryptojs.core.min.js"></script>
+<script src="scripts/vendors/cryptojs.md5.min.js"></script>
+<script src="scripts/datatable-init.js"></script>
+<script src="scripts/appsec/database.js"></script>
+
+<style>@import url("styles/jquery.dataTables.min.css");</style>
