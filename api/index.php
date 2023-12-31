8e38d30b4832f4e3f4cc376fbeba4a1c0a590cd8
nthnn <nathanneisip@gmail.com>
2023-12-31 15:09:53 +0800

    API gateway 'db_get_mode' action.

--- api/index.php
+++ api/index.php
@@ -1457,7 +1457,25 @@ if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] === "POST" &&
             array_push($args, $name, $mode);
             break;
 
-        default:
+        case "db_get_mode":
+            $backend = "database";
+            array_push($args, "get_db_mode", $apiKey);
+    
+            if(!isset($_POST["name"]) || empty($_POST["name"])) {
+                failedResponseMessage("Insufficient parameter arity.");
+                return;
+            }
+
+            $name = $_POST["name"];
+            if(!validateName($name)) {
+                failedResponseMessage("Invalid database name.");
+                return;
+            }
+
+            array_push($args, $name);
+            break;
+
+            default:
             failedResponse();
             return;
     }
