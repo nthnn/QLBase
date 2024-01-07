f937467d30f43b3a8988b2f71677a9c0b012d3b4
nthnn <nathanneisip@gmail.com>
2024-01-07 22:11:07 +0800

    Improved services content section in homepage.

--- views/home.php
+++ views/home.php
@@ -75,17 +75,17 @@
 </div>
 <br/>
 
-<div class="row">
-    <div class="col-6">
+<div class="row m-0 p-0">
+    <div class="col-6 m-0 p-0">
         <img src="./assets/screenshot_right.png" width="100%" />
     </div>
 
-    <div class="col-6">
+    <div class="col-6 m-0 p-0">
         <img src="./assets/screenshot_left.png" width="100%" />
     </div>
 </div>
 
-<div class="mobile-break">
+<div class="mobile-break d-block">
     <br/><br/>
 </div>
 
@@ -178,20 +178,92 @@
 
     <ul class="timeline">
         <li class="event" data-date="Authentication">
-            <p>QLBase provides a robust authentication system that allows you to manage user accounts securely.</p>
-
             <div class="row">
                 <div class="col-lg-6">
+                    <p>QLBase provides a robust authentication system that allows you to create, retrieve, update, and delete user accounts securely.</p>
+
                     <ul>
-                        <li><b>User Registration</b> &mdash; Easily register new users with customizable registration fields.</li>
-                        <li><b>Login</b> &mdash; Enable users to log in securely, supporting various authentication methods, including username/password and social logins.</li>
-                        <li><b>Account Enabling/Disabling</b> &mdash; You can disable and enable user accounts as needed. When an account is disabled, the user cannot log in until it's enabled again. This feature is particularly useful for handling suspicious or inactive accounts.</li>
-                        <li><b>Updating and Deleting Accounts</b> &mdash; Update or delete any user account with ease by either user's username and email.</li>
-                        <li><b>Account Fetching</b> &mdash; Flexibility in fetching user accounts; by specified username or email. You can also fetch all user informations in one-go.</li>
+                        <li>
+                            <b>User Registration</b>
+                            <p>Developers can easily integrate user registration, defining the necessary fields for user creation. This flexibility ensures that the registration process aligns with the unique needs of the application.</p>
+                        </li>
+                        <li>
+                            <b>Login and Authentication</b>
+                            <p>Users can securely log in using their credentials, with support for diverse authentication mechanisms. This ensures accessibility and convenience for users while maintaining the necessary security measures.</p>
+                        </li>
+                        <li>
+                            <b>Account Enabling/Disabling</b>
+                            <p>You can disable and enable user accounts as needed. When an account is disabled, the user cannot log in until it's enabled again. This feature is particularly useful for handling suspicious or inactive accounts.</p>
+                        </li>
+                        <li>
+                            <b>Updating and Deleting Accounts</b>
+                            <p>In QLBase, you can update user account details or delete accounts efficiently using either the user's username or email. This provides flexibility in managing user information based on the application's evolving requirements.</p></li>
+                        <li>
+                            <b>Account Fetching</b>
+                            <p>Flexibility in fetching user accounts; by specified username or email. Additionally, QLBase supports fetching all user information in a single request, providing a comprehensive overview of user accounts when needed.</p>
+                        </li>
                     </ul>
                 </div>
                 <div class="col-lg-6">
-                    <img src="./assets/qlbase-cat-1.png" width="100%" style="margin-top: -60px" />
+                    <img src="./assets/qlbase-cat-3.png" width="100%" />
+                </div>
+            </div>
+
+            <hr/>
+        </li>
+        <li class="event" data-date="SMS Verification">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>Easily integrate SMS verification into your application using QLBase's OTP service. Protect your user accounts with one-time passwords sent via SMS. QLBase provides SMS verification functionality using an Arduino UNO with a SIM900 Shield as the underlying SMS hardware. This setup allows you to seamlessly integrate SMS verification into your application for added security. </p>
+
+                    <ul>
+                        <li>
+                            <b>OTP Generation</b>
+                            <p>Utilizing the Arduino UNO with the SIM900 Shield, QLBase facilitates the secure generation of unique six-digit OTPs, ensuring a robust mechanism for user verification.</p>
+                        </li>
+                        <li>
+                            <b>SMS Delivery</b>
+                            <p>The integration with Arduino's SIM900 allows QLBase to send SMS messages efficiently, providing users with the necessary OTP for verification purposes. Including support contact details adds an extra layer of assistance.</p>
+                        </li>
+                        <li>
+                            <b>Verification Endpoint</b>
+                            <p>Users can interact with the verification endpoint, providing the OTP received via SMS to confirm their phone numbers. This API-driven verification process enhances the user experience by integrating with your application's workflows.</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <img src="./assets/qlbase-cat-4.png" width="100%" class="mt-4" />
+                </div>
+            </div>
+
+            <hr/>
+        </li>
+        <li class="event" data-date="Data Analytics">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>QLBase includes powerful data analytics capabilities such as track identification, general tracking with event name and JSON payloads, page tracking with a specified page name and category, and making alias for users and anonymous users.</p>
+
+                    <ul>
+                        <li>
+                            <b>Identification Trackers</b>
+                            <p>QLBase provides robust features for precise data identification, allowing you to associate user actions with specific individuals or anonymous IDs.</p>
+                        </li>
+                        <li>
+                            <b>General Analytics Tracking</b>
+                            <p>The data tracking feature captures a variety of user interactions, encompassing general tracking with event names and JSON payloads. This allows you to monitor specific events or actions performed by users, along with additional data payloads for more contextual information.</p>
+                        </li>
+                        <li>
+                            <b>Tracker Paging</b>
+                            <p>Paged tracking incorporates efficient data paging mechanisms, addressing the challenges associated with handling large datasets without compromising performance and is crucial for applications dealing with substantial datasets, preventing performance bottlenecks and enhancing user experience.</p>
+                        </li>
+                        <li>
+                            <b>Anonymous Users and Aliasing</b>
+                            <p>Data analytics and tracking also facilitates aliasing for both registered users and anonymous users, allowing you to create alternative identifiers or pseudonyms for them.</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6" align="center">
+                    <img src="./assets/qlbase-cat-5.png" width="80%" class="mt-4" />
                 </div>
             </div>
 
