c6bb3c99b993270e05f227afec95298237cc08d8
nthnn <nathanneisip@gmail.com>
2024-01-09 02:02:00 +0800

    File Storage backend database configuration.

---
+++ backend/storage/db_config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/nthnn/QLBase/storage/proc"
+	"gopkg.in/ini.v1"
+)
+
+func LoadDBConfig(configFile string) DBConfig {
+	ini, err := ini.Load(configFile)
+	if err != nil {
+		proc.ShowFailedResponse("Internal error occured.")
+		os.Exit(0)
+	}
+
+	database := ini.Section("database")
+	return ConfigDB(
+		database.Key("username").String(),
+		database.Key("password").String(),
+		database.Key("name").String(),
+		database.Key("server").String(),
+		uint16(database.Key("port").MustInt(3306)))
+}
