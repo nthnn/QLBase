0a89e5fc22b3ce074434b13493bb69e821e873db
nthnn <nathanneisip@gmail.com>
2024-01-16 22:23:39 +0800

    Database columns made 'NOT NULL'

--- db.sql
+++ db.sql
@@ -1,36 +1,43 @@
 CREATE TABLE `accounts` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 );
 
 CREATE TABLE `app` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `creator_id` int(11),
-  `app_id` varchar(255) DEFAULT NULL,
-  `app_key` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` varchar(1024) DEFAULT NULL
+  `app_id` varchar(255) NOT NULL,
+  `app_key` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1024) NOT NULL
+);
+
+CREATE TABLE `cdp` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `ticket` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
 );
 
 CREATE TABLE `recovery` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `track_id` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL
+  `track_id` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 );
 
 CREATE TABLE `sessions` (
-  `user_id` int(11) DEFAULT NULL,
-  `hash` varchar(255) DEFAULT NULL,
-  `user_agent` varchar(255) DEFAULT NULL,
-  `remote_addr` varchar(255) DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `user_agent` varchar(255) NOT NULL,
+  `remote_addr` varchar(255) NOT NULL
 );
 
 CREATE TABLE `traffic` (
-  `app_id` varchar(255) DEFAULT NULL,
-  `api_key` varchar(255) DEFAULT NULL,
+  `app_id` varchar(255) NOT NULL,
+  `api_key` varchar(255) NOT NULL,
   `date_time` varchar(255) DEFAULT DATE_FORMAT(CURRENT_DATE, '%d%m%Y'),
   `count` int(11) DEFAULT 1
 );
