8aea4a4ccfc49919c179f9468b83b8d4648c15ff
nthnn <nathanneisip@gmail.com>
2024-02-09 15:01:11 +0800

    Initial app settings layout on views.

--- views/appsec/settings.php
+++ views/appsec/settings.php
@@ -1,2 +1,28 @@
 <h1>Settings</h1>
 <hr/>
+
+<div class="form-group">
+    <div class="row">
+        <div class="col-3">
+            <label class="form-label" for="app-name">App Name</label>
+        </div>
+
+        <div class="col-9">
+            <input type="text" class="form-control" id="app-name" placeholder="App Name" />
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-3">
+            <label class="form-label" for="app-desc">App Description</label>
+        </div>
+
+        <div class="col-9">
+            <input type="text" class="form-control" id="app-desc" placeholder="App Description" />
+        </div>
+    </div>
+
+    <div align="right" class="mt-2">
+        <button class="btn btn-outline-primary px-4">Save</button>
+    </div>
+</div>
