b2855a72f71fa39064448c4850496cbd9f194f54
nthnn <nathanneisip@gmail.com>
2024-03-16 02:54:36 +0800

    Shell log recording functionality on execute().

--- controller/shell.php
+++ controller/shell.php
@@ -1,6 +1,18 @@
 <?php
+function logShelling($apiKey) {
+    $action = "N/A";
+    if(isset($_GET["action"]) && !empty($_GET["action"]))
+        $action = $_GET["action"];
+
+    shell_exec("\"../bin/logger\" \"".$apiKey."\" \"".$_SERVER["REMOTE_ADDR"].
+        "\" \"".$action."\" \"".date("Y-m-d H:i:s")."\" \"".
+        $_SERVER["HTTP_USER_AGENT"]."\"");
+}
+
 function execute($apiKey, $appId, $backend, $args) {
     logNetworkTraffic($apiKey, $appId);
-    echo shell_exec("../bin/".$backend." ".join(" ", $args));
+    logShelling($apiKey);
+
+    echo shell_exec("\"../bin/".$backend."\" ".join(" ", $args));
 }
 ?>
