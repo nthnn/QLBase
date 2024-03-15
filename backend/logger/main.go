6e98bbe935677c5091753ecca5bcce2d85b93cdc
nthnn <nathanneisip@gmail.com>
2024-03-16 02:53:24 +0800

    Some tweaks on logger backend source file.

--- backend/logger/main.go
+++ backend/logger/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 5 {
+	if len(os.Args) != 6 {
 		os.Exit(0)
 	}
 
 	args := os.Args[1:]
-	apiKey := args[1]
-	origin := args[2]
-	action := args[3]
-	dateTime := args[4]
-	userAgent := args[5]
+	apiKey := args[0]
+	origin := args[1]
+	action := args[2]
+	dateTime := args[3]
+	userAgent := args[4]
 
 	DispatchWithCallback(func(db *sql.DB) {
-		query, _ := db.Query("INSERT INTO qba_" + apiKey +
+		query, _ := db.Query("INSERT INTO " + apiKey +
 			"_logs (origin, action, datetime, user_agent) VALUES(\"" +
 			origin + "\", \"" + action + "\", \"" + dateTime + "\", \"" +
 			userAgent + "\")")
