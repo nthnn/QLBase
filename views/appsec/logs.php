cd4af8a7132ab7dd17237e4b12165036d56df806
nthnn <nathanneisip@gmail.com>
2024-03-16 01:10:47 +0800

    Initial record log page table on app.

--- views/appsec/logs.php
+++ views/appsec/logs.php
@@ -1,2 +1,22 @@
 <h1>Log Reports</h1>
 <hr/>
+
+<table id="logs-table" class="table table-hover w-100" cellspacing="0">
+    <thead>
+        <th>Origin</th>
+        <th>Action</th>
+        <th>Date/Time</th>
+        <th>User-Agent</th>
+        <th>Result</th>
+    </thead>
+    <tbody id="logs-table-body">
+    </tbody>
+</table>
+
+<script src="scripts/vendors/jquery.min.js"></script>
+<script src="scripts/vendors/jquery.dataTables.min.js"></script>
+<script src="scripts/vendors/dataTables.responsive.min.js"></script>
+<script src="scripts/datatable-init.js"></script>
+<script src="scripts/appsec/logs.js"></script>
+
+<style>@import url("styles/jquery.dataTables.min.css");</style>
