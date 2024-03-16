0e4aabdbe5ffb544a5b37e5559140b07a10b89b6
nthnn <nathanneisip@gmail.com>
2024-03-16 14:47:04 +0800

    Log record fetching API backend gateway.

---
+++ api/logs.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once("../controller/apps.php");
+include_once("../controller/validator.php");
+
+header('Content-Type: application/json; charset=utf-8');
+
+function failedResponse() {
+    echo "{\"result\": \"0\"}";
+}
+
+if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] === "POST" &&
+    isset($_SERVER["HTTP_QLBASE_API_KEY"]) && !empty($_SERVER["HTTP_QLBASE_API_KEY"]) &&
+    isset($_SERVER["HTTP_QLBASE_APP_ID"]) && !empty($_SERVER["HTTP_QLBASE_APP_ID"])) {
+    $key = $_SERVER["HTTP_QLBASE_API_KEY"];
+    if(!validateApiKey($key)) {
+        failedResponse();
+        return;
+    }
+
+    $id = $_SERVER["HTTP_QLBASE_APP_ID"];
+    if(!validateAppId($id)) {
+        failedResponse();
+        return;
+    }
+
+    echo shell_exec("\"../bin/logger\" dump ".$key);
+    return;
+}
+
+failedResponse();
+
+?>
