103f22bacb4feeb0396117c1e6594336930541b4
nthnn <nathanneisip@gmail.com>
2024-03-16 14:46:51 +0800

    Back-end for logger with 'dump' command.

--- backend/logger/main.go
+++ backend/logger/main.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"os"
 )
 
+func dumpLogs() {
+	DispatchWithCallback(func(db *sql.DB) {
+		query, err := db.Query("SELECT origin, action, datetime, user_agent FROM " + os.Args[2] + "_logs")
+		defer query.Close()
+
+		if err != nil {
+			os.Exit(0)
+		}
+
+		var logs [][]string
+		for query.Next() {
+			origin := ""
+			action := ""
+			dateTime := ""
+			userAgent := ""
+
+			query.Scan(&origin, &action, &dateTime, &userAgent)
+			logs = append(logs, []string {origin, action, dateTime, userAgent})
+		}
+
+		jsonData, _ := json.Marshal(logs)
+		fmt.Println(string(jsonData))
+	})
+}
+
 func main() {
+	if len(os.Args) == 3 {
+		dumpLogs()
+		os.Exit(0)
+	}
+
 	if len(os.Args) != 6 {
 		os.Exit(0)
 	}
