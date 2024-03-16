3f3fbf76d259afda63a02cdf78ecc8005b5e0f85
nthnn <nathanneisip@gmail.com>
2024-03-16 15:41:35 +0800

    Function log section in app dashboard.

--- scripts/appsec/logs.js
+++ scripts/appsec/logs.js
@@ -1,9 +1,9 @@
-let prevLogsHash = "";
+let prevLogsHash = "", prevHideState = true;
 let dataTable = null;
 
 const fetchLogs = ()=> {
-    /*$.post({
-        url: "api/index.php?action=file_fetch_all",
+    $.post({
+        url: "api/logs.php",
         headers: {
             "QLBase-App-ID": App.appId,
             "QLBase-API-Key": App.appKey
@@ -12,31 +12,46 @@ const fetchLogs = ()=> {
             if(data.result == '0')
                 return;
 
-            if(prevLogsHash == CryptoJS.MD5(JSON.stringify(data)).toString())
+            let hideSelfReqs = $("#hide-selfreqs").is(":checked");
+            if(prevHideState == hideSelfReqs &&
+                prevLogsHash == CryptoJS.MD5(JSON.stringify(data)).toString())
                 return;
-            prevLogsHash = CryptoJS.MD5(JSON.stringify(data)).toString();
 
-            if(data.value.length == 0 && (prevLogsHash != "" ||
+            prevLogsHash = CryptoJS.MD5(JSON.stringify(data)).toString();
+            prevHideState = hideSelfReqs;
+
+            if(data.length == 0 && (prevLogsHash != "" ||
                 prevLogsHash != "")) {
                 dataTable.clear().destroy();
-                dataTable = initDataTable("#storage-table", "No stored files found.");
+                dataTable = initDataTable("#logs-table", "No log records found.");
 
                 return;
             }
 
-            let fileRows = "";
-            for(let files of data.value) {
-                fileRows += "<tr><td>" + files[0].substring(1, 15) + "</td><td>" +
-                    files[1] + "</td><td>" + files[2] + "</td><td>" + files[3] + "</td><td><button class=\"btn btn-sm btn-outline-danger mx-2\"" +
-                    " onclick=\"deleteFile('" + files[0] + "', '" + files[1] +
-                    "')\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" widht=\"16\" height=\"16\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0\" /></svg></button></td></tr>";
+            let logRows = "";
+            data.reverse();
+            for(let log of data) {
+                let tag = log[4], badgeColor = "primary";
+                if(hideSelfReqs && tag == "dashboard")
+                    continue;
+
+                if(tag == "dashboard")
+                    badgeColor = "warning";
+                else if(tag == "sandbox")
+                    badgeColor = "success";
+
+                tag = tag.charAt(0).toUpperCase() + tag.slice(1);
+                logRows += "<tr><td>" + atob(log[0]) + "</td><td>" +
+                    atob(log[1]) + "</td><td>" + log[2] + "</td><td>" +
+                    atob(log[3]) + "</td><td><span class=\"badge bg-" + badgeColor +
+                    "\">" + tag + "</span></td></tr>";
             }
 
             dataTable.clear().destroy();
-            $("#storage-table-body").html(fileRows);
-            dataTable = initDataTable("#storage-table", "No stored files found.");
+            $("#logs-table-body").html(logRows);
+            dataTable = initDataTable("#logs-table", "No log records found.");
         }
-    });*/
+    });
 };
 
 $(document).ready(()=> {
--- views/appsec/logs.php
+++ views/appsec/logs.php
@@ -3,19 +3,27 @@
 
 <table id="logs-table" class="table table-hover w-100" cellspacing="0">
     <thead>
-        <th>Origin</th>
+        <th>Client</th>
         <th>Action</th>
         <th>Date/Time</th>
         <th>User-Agent</th>
-        <th>Result</th>
+        <th>Origin</th>
     </thead>
     <tbody id="logs-table-body">
     </tbody>
 </table>
 
+<hr/>
+<div class="w-100">
+    <input type="checkbox" id="hide-selfreqs" onclick="fetchLogs()" checked />
+    <label for="hide-selfreqs">Hide self-requests on logs</label>
+</div>
+
 <script src="scripts/vendors/jquery.min.js"></script>
 <script src="scripts/vendors/jquery.dataTables.min.js"></script>
 <script src="scripts/vendors/dataTables.responsive.min.js"></script>
+<script src="scripts/vendors/cryptojs.core.min.js"></script>
+<script src="scripts/vendors/cryptojs.md5.min.js"></script>
 <script src="scripts/datatable-init.js"></script>
 <script src="scripts/appsec/logs.js"></script>
 
