749b8ca49f415bf4d48ee359bb120b06b77a734a
nthnn <nathanneisip@gmail.com>
2024-04-07 04:16:45 +0800

    Reformatted controller/db_config.php

--- controller/db_config.php
+++ controller/db_config.php
@@ -1,8 +1,10 @@
 <?php
-    $db_server = "localhost";
-    $db_username = "root";
-    $db_password = "";
 
-    $db_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase");
-    $db_apps_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase_apps");
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "";
+
+$db_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase");
+$db_apps_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase_apps");
+
 ?>
