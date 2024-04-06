efc6595ccff2daf4d57e1f88faf547e70548ef0a
nthnn <nathanneisip@gmail.com>
2024-04-07 00:15:27 +0800

    Account class on server-side controller.

--- controller/account.php
+++ controller/account.php
@@ -12,111 +12,113 @@ enum CreateAccountResponse {
     case USERNAME_ALREADY_IN_USE;
 }
 
-function hasAccountForEmail($email) {
-    global $db_conn;
+class Account {
+    private static function hasAccountForEmail($email) {
+        global $db_conn;
 
-    return mysqli_num_rows(
-        mysqli_query(
+        return mysqli_num_rows(
+            mysqli_query(
+                $db_conn,
+                "SELECT * FROM accounts WHERE email=\"".$email."\""
+            )
+        ) != 0;
+    }
+
+    private static function hasAccountForUsername($username) {
+        global $db_conn;
+
+        return mysqli_num_rows(
+            mysqli_query(
+                $db_conn,
+                "SELECT * FROM accounts WHERE username=\"".$username."\""
+            )
+        ) != 0;
+    }
+
+    public static function create($name, $username, $email, $password) {
+        if(Account::hasAccountForEmail($email))
+            return CreateAccountResponse::EMAIL_ALREADY_IN_USE;
+
+        if(Account::hasAccountForUsername($username))
+            return CreateAccountResponse::USERNAME_ALREADY_IN_USE;
+
+        global $db_conn;
+        $result = mysqli_query(
             $db_conn,
-            "SELECT * FROM accounts WHERE email=\"".$email."\""
-        )
-    ) != 0;
-}
+            "INSERT INTO accounts (name, username, email, password) ".
+            "VALUES (\"".$name."\", \"".$username."\", \"".$email.
+            "\", \"".md5($password)."\")"
+        );
 
-function hasAccountForUsername($username) {
-    global $db_conn;
+        return $result ?
+            CreateAccountResponse::SUCCESS :
+            CreateAccountResponse::DB_ERROR;
+    }
 
-    return mysqli_num_rows(
-        mysqli_query(
+    public static function update($username, $name, $email, $password, $old) {
+        if(!Account::hasAccountForUsername($username))
+            return false;
+
+        global $db_conn;
+        $res = mysqli_query($db_conn, "UPDATE accounts SET name=\"".$name.
+            "\", email=\"".$email."\", password=\"".md5($password)."\" WHERE username=\"".
+            $username."\" AND password=\"".md5($old)."\" AND id=".getIdOfSession());
+
+        return !(!$res);
+    }
+
+    public static function login($username, $password) {
+        if(!Account::hasAccountForUsername($username))
+            return false;
+
+        global $db_conn;
+        $result = mysqli_query(
             $db_conn,
-            "SELECT * FROM accounts WHERE username=\"".$username."\""
-        )
-    ) != 0;
-}
+            "SELECT id FROM accounts WHERE ".
+            "username=\"".$username."\" AND password=\"".md5($password)."\""
+        );
 
-function createAccount($name, $username, $email, $password) {
-    if(hasAccountForEmail($email))
-        return CreateAccountResponse::EMAIL_ALREADY_IN_USE;
+        if(!$result || mysqli_num_rows($result) == 0)
+            return false;
 
-    if(hasAccountForUsername($username))
-        return CreateAccountResponse::USERNAME_ALREADY_IN_USE;
+        createSession(mysqli_fetch_array($result)[0]);
+        return true;
+    }
 
-    global $db_conn;
-    $result = mysqli_query(
-        $db_conn,
-        "INSERT INTO accounts (name, username, email, password) ".
-        "VALUES (\"".$name."\", \"".$username."\", \"".$email.
-        "\", \"".md5($password)."\")"
-    );
+    public static function getInfo($id) {
+        global $db_conn;
 
-    return $result ?
-        CreateAccountResponse::SUCCESS :
-        CreateAccountResponse::DB_ERROR;
-}
+        $res = mysqli_query(
+            $db_conn,
+            "SELECT name, email FROM accounts WHERE id=".$id
+        );
 
-function updateAccount($username, $name, $email, $password, $old) {
-    if(!hasAccountForUsername($username))
-        return false;
+        if(!$res || mysqli_num_rows($res) != 1)
+            return null;
 
-    global $db_conn;
-    $res = mysqli_query($db_conn, "UPDATE accounts SET name=\"".$name.
-        "\", email=\"".$email."\", password=\"".md5($password)."\" WHERE username=\"".
-        $username."\" AND password=\"".md5($old)."\" AND id=".getIdOfSession());
+        $results = mysqli_fetch_array($res);
+        $array = array($results[0], $results[1]);
 
-    return !(!$res);
-}
+        mysqli_free_result($res);
+        return $array;
+    }
 
-function loginAccount($username, $password) {
-    if(!hasAccountForUsername($username))
-        return false;
+    public static function getUsername($id) {
+        global $db_conn;
 
-    global $db_conn;
-    $result = mysqli_query(
-        $db_conn,
-        "SELECT id FROM accounts WHERE ".
-        "username=\"".$username."\" AND password=\"".md5($password)."\""
-    );
+        $res = mysqli_query(
+            $db_conn,
+            "SELECT username FROM accounts WHERE id=".$id
+        );
 
-    if(!$result || mysqli_num_rows($result) == 0)
-        return false;
+        if(!$res || mysqli_num_rows($res) != 1)
+            return null;
 
-    createSession(mysqli_fetch_array($result)[0]);
-    return true;
-}
+        $username = mysqli_fetch_array($res)[0];
+        mysqli_free_result($res);
 
-function getAccountInfo($id) {
-    global $db_conn;
-
-    $res = mysqli_query(
-        $db_conn,
-        "SELECT name, email FROM accounts WHERE id=".$id
-    );
-
-    if(!$res || mysqli_num_rows($res) != 1)
-        return null;
-
-    $results = mysqli_fetch_array($res);
-    $array = array($results[0], $results[1]);
-
-    mysqli_free_result($res);
-    return $array;
-}
-
-function getAccountUsername($id) {
-    global $db_conn;
-
-    $res = mysqli_query(
-        $db_conn,
-        "SELECT username FROM accounts WHERE id=".$id
-    );
-
-    if(!$res || mysqli_num_rows($res) != 1)
-        return null;
-
-    $username = mysqli_fetch_array($res)[0];
-    mysqli_free_result($res);
-
-    return $username;
+        return $username;
+    }
 }
 
 ?>
