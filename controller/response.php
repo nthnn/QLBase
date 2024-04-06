1b282716ce7ac4464295124cfd42b83728b80073
nthnn <nathanneisip@gmail.com>
2024-04-07 07:09:21 +0800

    Response class on server-side controller.

---
+++ controller/response.php
@@ -0,0 +1,24 @@
+<?php
+
+class Response {
+    private static function jsonContent() {
+        header("Content-Type: application/json; charset=utf-8");
+    }
+
+    public static function failed() {
+        Response::jsonContent();
+        echo "{\"result\": \"0\"}";
+    }
+    
+    public static function failedMessage($message) {
+        Response::jsonContent();
+        echo "{\"result\": \"0\", \"message\": \"".$message."\"}";
+    }
+
+    public static function success() {
+        Response::jsonContent();
+        echo "{\"result\": \"1\"}";
+    }
+}
+
+?>
