c134eb770900451764da578c785396b77078d416
nthnn <nathanneisip@gmail.com>
2024-04-07 05:25:20 +0800

    Fixed private function error on Util class from controllers.

--- controller/util.php
+++ controller/util.php
@@ -2,17 +2,15 @@
 
 class Util {
     // From https://www.uuidgenerator.net/dev-corner/php
-    private static function guidv4($data) {
+    public static function guidv4($data) {
         $data = $data ?? random_bytes(16);
-        assert(strlen($data) == 16);
-
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
-    private static function generateAppID() {
+    public static function generateAppID() {
         $bytes = random_bytes(8);
         $hexStr = bin2hex($bytes);
 
