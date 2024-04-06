be6aeb53de143bb369e5a57f84c46663e2243bc9
nthnn <nathanneisip@gmail.com>
2024-04-07 04:20:37 +0800

    Util class on server-side controller.

--- controller/util.php
+++ controller/util.php
@@ -1,24 +1,26 @@
 <?php
 
-// From https://www.uuidgenerator.net/dev-corner/php
-function guidv4($data = null) {
-    $data = $data ?? random_bytes(16);
-    assert(strlen($data) == 16);
+class Util {
+    // From https://www.uuidgenerator.net/dev-corner/php
+    private static function guidv4($data) {
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
 
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-}
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 
-function generateAppID() {
-    $bytes = random_bytes(8);
-    $hexStr = bin2hex($bytes);
+    private static function generateAppID() {
+        $bytes = random_bytes(8);
+        $hexStr = bin2hex($bytes);
 
-    return substr($hexStr, 0, 4) . '-' .
-        substr($hexStr, 4, 4) . '-' .
-        substr($hexStr, 8, 4) . '-' .
-        substr($hexStr, 12, 4);
+        return substr($hexStr, 0, 4) . '-' .
+            substr($hexStr, 4, 4) . '-' .
+            substr($hexStr, 8, 4) . '-' .
+            substr($hexStr, 12, 4);
+    }
 }
 
 ?>
