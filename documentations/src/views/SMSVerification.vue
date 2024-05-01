e4644c6cd253f38d63466064a622df5f742ba377
nthnn <nathanneisip@gmail.com>
2024-05-02 00:18:37 +0800

    Fixed SMS Verification page bottom navigation buttons.

--- documentations/src/views/SMSVerification.vue
+++ documentations/src/views/SMSVerification.vue
@@ -25,20 +25,20 @@
     <hr/>
     <div class="row">
         <div class="col-6">
-            <RouterLink to="/intro-api" class="btn btn-primary">
+            <RouterLink to="/authentication" class="btn btn-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right mb-1" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                     <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                 </svg>
 
-                Introduction to APIs
+                Authentication
             </RouterLink>
         </div>
 
         <div class="col-6">
             <div align="right">
-                <RouterLink to="/sms-verification" class="btn btn-primary">
-                    SMS Verification
+                <RouterLink to="/data-analytics" class="btn btn-primary">
+                    Data Analytics
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right mb-1" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
