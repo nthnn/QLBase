62086e85d910cc8e49dc9d286114c9eedff26b2c
nthnn <nathanneisip@gmail.com>
2024-05-05 01:57:22 +0800

    Deployment page GitHub link.

--- documentations/src/views/Deployment.vue
+++ documentations/src/views/Deployment.vue
@@ -1,6 +1,25 @@
 <template>
-    <h1 class="border-bottom">🚀 Deployment</h1>
-    <div class="row">
+    <div class="row border-bottom">
+        <div class="col-lg-6">
+            <h1>🚀 Deployment</h1>
+        </div>
+
+        <div class="col-lg-6 desktop-only" align="right">
+            <div class="pt-4">
+                <a href="https://github.com/nthnn/QLBase/blob/main/documentations/src/views/Deployment.vue"
+                    target="_blank" class="text-decoration-none">
+                    <span style="margin-top: 2px !important; display: flex; float: right">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                    </span>
+                    <span class="float-right d-inline mx-2">Edit this on GitHub</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-2">
         <div class="col-md-6">
             <p>Deploying QLBase is a straightforward and hassle-free process that can be executed on virtually any system, provided it possesses the capability to run both the Apache and MySQL server, two fundamental components for QLBase to operate seamlessly.</p>
             <p>To initiate the deployment, all you need to do is follow the comprehensive instructional guide outlined below, ensuring that each step is executed with precision and attention to detail.</p>
