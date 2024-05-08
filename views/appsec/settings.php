2d529d42dd1ee1424c9f934c123074a09511ccba
nthnn <nathanneisip@gmail.com>
2024-05-09 00:56:15 +0800

    Fixed app deletion modal dialog.

--- views/appsec/settings.php
+++ views/appsec/settings.php
@@ -47,7 +47,7 @@ $appInfo = Apps::getInfoById(SessionControl::getId(), $_GET["id"]);
                 </div>
 
                 <div class="col-md-6">
-                    <button type="button" class="btn btn-outline-danger w-100" data-bs-toggle="modal" data-bs-target="#delete-modal">
+                    <button type="button" id="show-delete-modal" class="btn btn-outline-danger w-100">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                         </svg>
@@ -78,7 +78,7 @@ $appInfo = Apps::getInfoById(SessionControl::getId(), $_GET["id"]);
                 <label for="deletion-password" class="form-label mt-2">Password</label>
                 <input type="password" id="deletion-password" class="form-control" placeholder="Password" />
 
-                <p class="text-danger d-none mt-2" id="deletion-error"></p>
+                <p class="text-danger d-none mt-2 mb-0 pb-0" id="deletion-error"></p>
             </div>
 
             <div class="modal-footer">
@@ -101,4 +101,5 @@ $appInfo = Apps::getInfoById(SessionControl::getId(), $_GET["id"]);
 </div>
 
 <script src="scripts/vendors/jquery.min.js"></script>
+<script src="scripts/rotating-button.js"></script>
 <script src="scripts/appsec/settings.js"></script>
