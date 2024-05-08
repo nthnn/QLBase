997d89ec6c50a296bde49fcc0afc71751dca2f95
nthnn <nathanneisip@gmail.com>
2024-05-08 18:00:42 +0800

    Log downloading functionality.

--- scripts/appsec/logs.js
+++ scripts/appsec/logs.js
@@ -1,5 +1,36 @@
-let prevLogsHash = "", prevHideState = true;
+let prevLogsHash = "",
+    prevLogsContents = "",
+    prevHideState = true;
 let dataTable = null;
+
+function downloadContent(fileName, content) {
+    let file = new File(
+        ["\ufeff" + content],
+        fileName,
+        {type: "text/plain:charset=UTF-8"}
+    );
+
+    let url = window.URL.createObjectURL(file);
+    let a = document.createElement("a");
+
+    a.style = "display: none";
+    a.href = url;
+    a.download = file.name;
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+}
+
+function downloadLogs() {
+    let content = "";
+    for(let row of prevLogsContents)
+        content += "\"" + atob(row[0]) + "\"," +
+            atob(row[1]) + "\"," +
+            row[2] + ",\"" +
+            atob(row[3]) + "\"\n";
+
+    downloadContent("logs.csv", content);
+}
 
 const fetchLogs = ()=> {
     $.post({
@@ -17,8 +48,9 @@ const fetchLogs = ()=> {
                 prevLogsHash == CryptoJS.MD5(JSON.stringify(data)).toString())
                 return;
 
-            prevLogsHash = CryptoJS.MD5(JSON.stringify(data)).toString();
+            prevLogsContents = data;
             prevHideState = hideSelfReqs;
+            prevLogsHash = CryptoJS.MD5(JSON.stringify(data)).toString();
 
             if(data.length == 0 && (prevLogsHash != "" ||
                 prevLogsHash != "")) {
