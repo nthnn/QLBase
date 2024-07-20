bae3687d6b77b37f3f6e2e5e9cd9f0caae392e6b
Nathanne Isip <nathanneisip@gmail.com>
2024-07-21 04:25:45 +0800

    Remove modal and settings on shared accessors.

--- views/appsec/settings.php
+++ views/appsec/settings.php
@@ -37,11 +37,11 @@ $appId = $appInfo["app_id"];
         <button class="btn btn-outline-primary px-4" id="settings-save">Save</button>
     </div>
 
-    <?php if(Apps::owner($appId)) { ?>
     <hr/>
     <div class="card border-danger mt-2">
         <div class="card-title bg-danger text-white py-1 px-2">Danger Zone</div>
         <div class="card-body">
+            <?php if(Apps::owner($appId)) { ?>
             <div class="row">
                 <div class="col-md-6">
                     <b>Delete this app</b>
@@ -57,9 +57,46 @@ $appId = $appInfo["app_id"];
                     </button>
                 </div>
             </div>
+            <?php
+                }
+                else {
+            ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <b>Delete this app</b>
+                    <p>Your shared access to this app will be removed. To regain access, you must contact the owner.</p>
+                </div>
+
+                <div class="col-md-6">
+                    <button type="button" id="show-remove-modal" class="btn btn-outline-danger w-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                        Delete App
+                    </button>
+                </div>
+            </div>
+            <?php
+                }
+            ?>
         </div>
     </div>
-    <?php } ?>
+</div>
+
+<div class="modal fade" id="remove-modal" tabindex="-1" role="dialog" aria-labelledby="remove-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="remove-modalLabel">Remove Modal</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to remove your shared access to this app?</p>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="delete-modalLabel" aria-hidden="true">
