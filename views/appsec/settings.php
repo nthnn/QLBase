678b6a3ba1f059dde1ef7f1fe93d2d2d303d45d9
Nathanne Isip <nathanneisip@gmail.com>
2024-07-23 01:04:06 +0800

    Modal dialog for failed and success message on user's access removal.

--- views/appsec/settings.php
+++ views/appsec/settings.php
@@ -114,7 +114,7 @@ $appId = $appInfo["app_id"];
     <div class="modal-dialog shadow" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="remove-modalLabel">Remove Modal</h5>
+                <h5 class="modal-title" id="remove-modalLabel">Remove Access</h5>
                 <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -145,7 +145,7 @@ $appId = $appInfo["app_id"];
     <div class="modal-dialog shadow" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="delete-modalLabel">Delete Modal</h5>
+                <h5 class="modal-title" id="delete-modalLabel">Delete</h5>
                 <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -186,7 +186,7 @@ $appId = $appInfo["app_id"];
     <div class="modal-dialog shadow" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="access-shared-modalLabel">Access Shared Modal</h5>
+                <h5 class="modal-title" id="access-shared-modalLabel">Access Shared</h5>
                 <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -210,7 +210,7 @@ $appId = $appInfo["app_id"];
     <div class="modal-dialog shadow" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="share-access-modalLabel">Share Access Modal</h5>
+                <h5 class="modal-title" id="share-access-modalLabel">Share Access</h5>
                 <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -243,6 +243,85 @@ $appId = $appInfo["app_id"];
                         <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3"/>
                     </svg>
                     Share
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="remove-access-modal" tabindex="-1" role="dialog" aria-labelledby="remove-access-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="remove-access-modalLabel">Remove Access</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to remove the access for this user?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+
+                <button type="button" class="btn btn-outline-danger" onclick="removeAccess()">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="remove-access-failed-modal" tabindex="-1" role="dialog" aria-labelledby="remove-access-failed-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="remove-access-failed-modalLabel">Remove Access Failed</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Failed to remove user's access to this application dashboard. Please try again.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="remove-access-success-modal" tabindex="-1" role="dialog" aria-labelledby="remove-access-success-modalLabel" aria-hidden="true">
+    <div class="modal-dialog shadow" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="remove-access-success-modalLabel">Remove Access Success</h5>
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>User's access to this application dashboard was successfully removed.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18" class="mb-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    Close
                 </button>
             </div>
         </div>
