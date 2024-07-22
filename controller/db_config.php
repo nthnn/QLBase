bbc81072c5a5df54cc262fa1e1c26ff2ae578277
Nathanne Isip <nathanneisip@gmail.com>
2024-07-23 01:30:58 +0800

    Configuration based database configuration on controller.

--- controller/db_config.php
+++ controller/db_config.php
@@ -1,10 +1,60 @@
 <?php
 
-$db_server = "localhost";
-$db_username = "root";
-$db_password = "";
+$config_file = parse_ini_file("bin/config.ini", true);
 
-$db_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase");
-$db_apps_conn = mysqli_connect($db_server, $db_username, $db_password, "qlbase_apps");
+class DBConfig {
+    public static function getServerAddress() {
+        if(isset($config["database"]["server"]))
+            return $config["database"]["server"];
+
+        return "localhost";
+    }
+
+    public static function getServerUsername() {
+        if(isset($config["database"]["username"]))
+            return $config["database"]["username"];
+
+        return "root";
+    }
+
+    public static function getServerPassword() {
+        if(isset($config["database"]["password"]))
+            return $config["database"]["password"];
+
+        return "";
+    }
+
+    public static function getInternalDatabaseName() {
+        if(isset($config["database"]["system"]))
+            return $config["database"]["system"];
+
+        return "qlbase";
+    }
+
+    public static function getAppDatabaseName() {
+        if(isset($config["database"]["name"]))
+            return $config["database"]["name"];
+
+        return "qlbase_apps";
+    }
+}
+
+$db_server = DBConfig::getServerAddress();
+$db_username = DBConfig::getServerUsername();
+$db_password = DBConfig::getServerPassword();
+
+$db_conn = mysqli_connect(
+    $db_server,
+    $db_username,
+    $db_password,
+    DBConfig::getInternalDatabaseName()
+);
+
+$db_apps_conn = mysqli_connect(
+    $db_server,
+    $db_username,
+    $db_password,
+    DBConfig::getAppDatabaseName()
+);
 
 ?>
