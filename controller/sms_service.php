434ecf78721ad0d495bdbb5f66574041e3dd0699
Nathanne Isip <nathanneisip@gmail.com>
2024-07-25 00:04:54 +0800

    Removed sms_service on controller, moved to Config class.

--- controller/sms_service.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function isSMSServiceEnabled() {
-    $config = parse_ini_file("bin/config.ini", true);
-    return isset($config["env"]["sms"]) &&
-        $config["env"]["sms"] === "enabled";
-}
-
-?>
